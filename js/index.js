5d02c8aef724dd67917e95e77797c7fd74cf15f9
rukia <ruqayashar1@gmail.com>
2023-04-06 22:42:55 +0300

    fetching from API

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
-console.log('linked js');
-// we get the element from the DOM
-let input = document.getElementById(input_box)
+// look for DOM Element
+// Get the book to borrow
+// post request
+const input = document.getElementById("input_box");
+
+function handleBorrow(){
+    let action = input.value;
+    console.log(action);
+    // gives an alert message "please enter book name" if the name was not entered into the textbox
+    let d= new Date(Date.now());
+    if(action === ""){
+        alert ("please Enter the book name");
+        return
+    }
+let data={
+
+ "date": 334556,
+"book_name": "string",
+"author": "string",
+"action": "borrow",
+"student": "string"
+}
+data=JSON.stringify(data)
+let url = "http://localhost:3000/book_data";
+fetch(url,{method:"POST", body:data})
+.then ((response)=>response.json())
+.then ((data)=>{
+input.value="";
+alert ('borrowed');
+});
+}
+
+handleBorrow()
+
+ 
