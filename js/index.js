ec2c60d8a83af39aa43f3cc0f6255b23b9807cf1
rukia <ruqayashar1@gmail.com>
2023-04-10 19:14:59 +0300

    display data on table

--- js/index.js
+++ js/index.js
@@ -2,28 +2,30 @@
 // Get the book to borrow
 // post request
 const bookInput = document.getElementById("input_box");
-const tableData=document.getElementById("table_data")
+const tableBody=document.getElementById("table_Body")
 const addBookButton = document.getElementById("addButton")
 const baseUrl = 'https://openlibrary.org/search.json';
 const searchForm=document.getElementById("searchForm")
-
+const studentNameBox=document.getElementById('studentName')
 
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault()
-    console.log("Button clicked, but the default action was prevented");
     let bookName = bookInput.value
     const apiUrl =  `${baseUrl}?q=${encodeURIComponent(bookName)}`
     console.log(apiUrl)
-    if(bookName){
-        searchBook(apiUrl)
+    let studentName = studentNameBox.value
+    console.log(studentName)
+    if(bookName && studentName){
+        searchBook(apiUrl,studentName)
     } else {
-        alert ("please Enter the book name");
+        alert ("please Enter the book name and student name");
     } 
     }
+    
 )
 
 //
-async function searchBook(apiUrl){
+async function searchBook(apiUrl,studentName){
     fetch(apiUrl)
     .then ((response)=>response.json())
     .then ((data)=>{
@@ -33,5 +35,23 @@ async function searchBook(apiUrl){
         console.log(bookTitle)
         const author = book.author_name[0];
         console.log(author)
+        
+        let tableRow = document.createElement('tr')
+        let cellOne = document.createElement('td')
+        let cellTwo = document.createElement('td')
+        let cellThree = document.createElement('td')
+        let cellFour = document.createElement('td')
+
+        let date= new Date()
+        let shortDate = date.toDateString()
+        cellOne.textContent= shortDate
+        tableRow.appendChild(cellOne)
+        cellTwo.textContent=bookTitle
+        tableRow.appendChild(cellTwo)
+        cellThree.textContent=author
+        tableRow.appendChild(cellThree)
+        cellFour.textContent=studentName
+        tableRow.appendChild(cellFour)
+        tableBody.appendChild(tableRow)
     })
 }
